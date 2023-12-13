272d15064e62eae9c422f7cc320f5aaf9359d396
Connor Keogh <contact@kyoh.run>
2023-12-13 21:17:19 +1100

    Cleanup comments

--- cmd/timezone.go
+++ cmd/timezone.go
@@ -11,8 +11,8 @@ var timezoneCmd = &cobra.Command{
 	Use:   "timezone",
 	Short: "Print the current time in a given timezone",
 	Long: `Print the current time in a given timezone.
-				This command takes one argument, the timezone you want to get the current time in.
-				It returns the current time in RFC1123 format.`,
+This command takes one argument, the timezone you want to get the current time in.
+It returns the current time in RFC1123 format.`,
 	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		timezone := args[0]
@@ -26,14 +26,4 @@ var timezoneCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(timezoneCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// timezoneCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// timezoneCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
